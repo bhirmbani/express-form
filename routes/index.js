bd817760888af0160e15010022be66377be0b1e1
akbarsahata <akbarsahata@outlook.co.id>
2017-03-16 22:47:03 +0700

    CREATE implementatiion

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  db.Post
+    .findAll({
+      order: [['createdAt', 'DESC']]
+    })
+    .then((posts) => {
+      res.render('index', { title: 'Single Form', data: posts  });
+    })
+    .catch((error) => {
+      res.send({ "error_message": err.message });
+    });
+});
+
+/* POST form submit */
+router.post('/add', (req, res, next) => {
+  db.Post
+    .create(req.body)
+    .then((post) => {
+      if (post) {
+        res.redirect('/')
+      } else {
+        res.send({ "error_message": "insert query failed!" });
+      }
+    })
+    .catch((err) => {
+      res.send({ "error_message": err.message });
+    })
 });
 
 module.exports = router;
